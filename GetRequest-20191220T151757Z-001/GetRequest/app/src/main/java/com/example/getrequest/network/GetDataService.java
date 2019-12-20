b17e3e4dbbd0d9c8205784bd2cc5be99d49f18a9
Mithun <monermithun789@gmail.com>
2019-12-20 08:02:12 -0800

    Initial commit

---
+++ GetRequest-20191220T151757Z-001/GetRequest/app/src/main/java/com/example/getrequest/network/GetDataService.java
@@ -0,0 +1,15 @@
+package com.example.getrequest.network;
+
+
+import com.example.getrequest.model.RetroPhoto;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+
+public interface GetDataService {
+
+    @GET("/photos")
+    Call<List<RetroPhoto>> getAllPhotos();
+}
